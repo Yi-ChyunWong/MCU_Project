1ee6a21136cf425524b0c61a7a7b521694f4d03b
Yi-Chyun Wong <yichyun.wong@gmail.com>
2026-07-23 23:16:10 -0400

    Completed alu tb

--- MCU_Project.sim/sim_1/behav/xsim/xsim.dir/alu_tb_behav/obj/xsim_1.c
+++ MCU_Project.sim/sim_1/behav/xsim/xsim.dir/alu_tb_behav/obj/xsim_1.c
@@ -54,28 +54,32 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_3(char*, char *);
-IKI_DLLESPEC extern void execute_11(char*, char *);
-IKI_DLLESPEC extern void execute_12(char*, char *);
-IKI_DLLESPEC extern void execute_13(char*, char *);
-IKI_DLLESPEC extern void execute_5(char*, char *);
-IKI_DLLESPEC extern void execute_7(char*, char *);
-IKI_DLLESPEC extern void execute_8(char*, char *);
-IKI_DLLESPEC extern void execute_9(char*, char *);
-IKI_DLLESPEC extern void execute_10(char*, char *);
-IKI_DLLESPEC extern void execute_14(char*, char *);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void execute_16(char*, char *);
-IKI_DLLESPEC extern void execute_17(char*, char *);
-IKI_DLLESPEC extern void execute_18(char*, char *);
-IKI_DLLESPEC extern void execute_19(char*, char *);
+IKI_DLLESPEC extern void execute_42(char*, char *);
+IKI_DLLESPEC extern void execute_47(char*, char *);
+IKI_DLLESPEC extern void execute_53(char*, char *);
+IKI_DLLESPEC extern void execute_54(char*, char *);
+IKI_DLLESPEC extern void execute_55(char*, char *);
+IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_for_reg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_57(char*, char *);
+IKI_DLLESPEC extern void execute_46(char*, char *);
+IKI_DLLESPEC extern void execute_49(char*, char *);
+IKI_DLLESPEC extern void execute_50(char*, char *);
+IKI_DLLESPEC extern void execute_51(char*, char *);
+IKI_DLLESPEC extern void execute_52(char*, char *);
+IKI_DLLESPEC extern void execute_58(char*, char *);
+IKI_DLLESPEC extern void execute_59(char*, char *);
+IKI_DLLESPEC extern void execute_60(char*, char *);
+IKI_DLLESPEC extern void execute_61(char*, char *);
+IKI_DLLESPEC extern void execute_62(char*, char *);
+IKI_DLLESPEC extern void execute_63(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[16] = {(funcp)execute_3, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_5, (funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)execute_17, (funcp)execute_18, (funcp)execute_19, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 16;
+IKI_DLLESPEC extern void transaction_81(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[20] = {(funcp)execute_42, (funcp)execute_47, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)vlog_simple_process_execute_0_fast_for_reg, (funcp)execute_57, (funcp)execute_46, (funcp)execute_49, (funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_58, (funcp)execute_59, (funcp)execute_60, (funcp)execute_61, (funcp)execute_62, (funcp)execute_63, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_81};
+const int NumRelocateId= 20;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/alu_tb_behav/xsim.reloc",  (void **)funcTab, 16);
+	iki_relocate(dp, "xsim.dir/alu_tb_behav/xsim.reloc",  (void **)funcTab, 20);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
@@ -87,6 +91,7 @@ void sensitize(char *dp)
 
 void simulate(char *dp)
 {
+iki_register_root_pointers(4, 6288, 12,0,0,5736, 13,2, 0, 32,6104, 14,0,0,5920, 13,2, 0, 32) ; 
 		iki_schedule_processes_at_time_zero(dp, "xsim.dir/alu_tb_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
@@ -96,6 +101,8 @@ void simulate(char *dp)
 
 }
 #include "iki_bridge.h"
+void subprog_m_ebc6a968_3ed2920_1() ;
+static char* ng20[] = {(void *)subprog_m_ebc6a968_3ed2920_1};
 void relocate(char *);
 
 void sensitize(char *);
@@ -112,6 +119,7 @@ int main(int argc, char **argv)
     iki_set_xsimdir_location_if_remapped(argc, argv)  ;
     iki_set_sv_type_file_path_name("xsim.dir/alu_tb_behav/xsim.svtype");
     iki_set_crvs_dump_file_path_name("xsim.dir/alu_tb_behav/xsim.crvsdump");
+    iki_svlog_initialize_virtual_tables(1, 2, ng20);
     void* design_handle = iki_create_design("xsim.dir/alu_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
--- MCU_Project.srcs/sim_1/new/alu_tb.sv
+++ MCU_Project.srcs/sim_1/new/alu_tb.sv
@@ -19,75 +19,269 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module alu_tb(
-);
-    logic [31:0] in1, in2;
+interface alu_if #(parameter WIDTH = 32) ();
+    logic [WIDTH-1:0] in1;
+    logic [WIDTH-1:0] in2;
     aluOp_t aluOp;
-    logic [31:0] out; 
+    logic [WIDTH-1:0] out;
+endinterface
 
-    initial begin
-        // Test ADD operation
-        in1 = 32'd10;
-        in2 = 32'd5;
-        aluOp = ADD;
-        #10;
-        $display("ADD: %d + %d = %d", in1, in2, out);
+class alu_transaction #(parameter WIDTH = 32);
+    rand logic [WIDTH-1:0] in1;
+    rand logic [WIDTH-1:0] in2;
+    rand aluOp_t aluOp;
 
-        // Test SUB operation
-        aluOp = SUB;
-        #10;
-        $display("SUB: %d - %d = %d", in1, in2, out);
+    logic [WIDTH-1:0] out;
 
-        // Test AND operation
-        aluOp = AND;
-        #10;
-        $display("AND: %d & %d = %d", in1, in2, out);
+    // constraint small_numbers {
+    //     in1 inside {[0:100]};
+    //     in2 inside {[0:100]};
+    // }
 
-        // Test OR operation
-        aluOp = OR;
-        #10;
-        $display("OR: %d | %d = %d", in1, in2, out);
+    function void display();
+        $display("Transaction: in1=%0h, in2=%0h, aluOp=%s", in1, in2, aluOp.name());
+    endfunction
 
-        // Test XOR operation
-        aluOp = XOR;
-        #10;
-        $display("XOR: %d ^ %d = %d", in1, in2, out);
+endclass
 
-        // Test SLL operation
-        aluOp = SLL;
-        #10;
-        $display("SLL: %d << %d = %d", in1, in2[4:0], out);
+class driver #(parameter WIDTH = 32);
+    virtual alu_if #(WIDTH) vif;
+    event drv_done;
+    mailbox #(alu_transaction #(WIDTH)) drv_mb;
+    alu_transaction #(WIDTH) tr;
 
-        // Test SRL operation
-        aluOp = SRL;
-        #10;
-        $display("SRL: %d >> %d = %d", in1, in2[4:0], out);
+    function new(
+        virtual alu_if #(WIDTH) vif, 
+        mailbox #(alu_transaction #(WIDTH)) drv_mb,
+        event drv_done
+    );
+        this.vif = vif;
+        this.drv_mb = drv_mb;
+        this.drv_done = drv_done;
+    endfunction
 
-        // Test SRA operation
-        aluOp = SRA;
-        #10;
-        $display("SRA: %d >>> %d = %d", in1, in2[4:0], out);    
+    task run();
+        $display("T=%0t: Driver starting", $time);
 
-        // Test SLT operation
-        aluOp = SLT;
-        #10;
-        $display("SLT: %d < %d = %d", in1, in2, out);
+        forever begin
+            drv_mb.get(tr);
 
-        // Test SLTU operation
-        aluOp = SLTU;
-        #10;
-        $display("SLTU: %d < %d = %d", in1, in2, out);
-        $finish;
-    end
+            vif.in1 = tr.in1;
+            vif.in2 = tr.in2;
+            vif.aluOp = tr.aluOp;
 
-    alu uut (
-        .in1(in1),
-        .in2(in2),
-        .aluOp(aluOp),
-        .out(out)
+            #1;
+
+            -> drv_done;
+
+        end
+    endtask
+endclass
+
+class generator #(parameter WIDTH = 32);
+    mailbox #(alu_transaction #(WIDTH)) drv_mb;
+    alu_transaction #(WIDTH) tr;
+    event drv_done;
+
+    function new(
+        mailbox #(alu_transaction #(WIDTH)) drv_mb,
+        event drv_done
+    );
+        this.drv_mb = drv_mb;
+        this.drv_done = drv_done;
+    endfunction
+
+    task run(int num_transactions = 100);
+        repeat (num_transactions) begin
+            tr = new();
+            assert(tr.randomize());
+            drv_mb.put(tr);
+            @(drv_done);
+        end
+    endtask
+
+endclass
+
+class monitor #(parameter WIDTH = 32);
+    virtual alu_if #(WIDTH) vif;
+    mailbox #(alu_transaction #(WIDTH)) mon_mb;
+    alu_transaction #(WIDTH) tr;
+    event drv_done;
+
+    function new(
+        virtual alu_if #(WIDTH) vif,
+        mailbox #(alu_transaction #(WIDTH)) mon_mb,
+        event drv_done
+    );
+        this.vif = vif;
+        this.mon_mb = mon_mb;
+        this.drv_done = drv_done;
+    endfunction
+
+    task run();
+        $display("T=%0t: Monitor starting", $time);
+
+        forever begin
+            @(drv_done);
+
+            #1;
+
+            tr = new();
+
+            tr.in1 = vif.in1;
+            tr.in2 = vif.in2;
+            tr.aluOp = vif.aluOp;
+            tr.out = vif.out;
+
+            mon_mb.put(tr);
+        end
+
+    endtask
+
+endclass
+
+class scoreboard #(parameter WIDTH=32);
+    mailbox #(alu_transaction #(WIDTH)) mon_mb;
+    alu_transaction #(WIDTH) tr;
+    event done;
+
+    logic [WIDTH-1:0] expected;
+
+    int pass_count;
+    int fail_count;
+    int total_count;
+
+    function new(mailbox #(alu_transaction #(WIDTH)) mon_mb);
+        this.mon_mb = mon_mb;
+
+        pass_count  = 0;
+        fail_count  = 0;
+        total_count = 0;
+    endfunction
+
+    task run(int num_transactions = 100);
+        repeat (num_transactions) begin
+            mon_mb.get(tr);
+            $display("Scoreboard received %0d", total_count+1);
+            case(tr.aluOp)
+                ADD: expected = tr.in1 + tr.in2;
+                SUB: expected = tr.in1 - tr.in2;
+                AND: expected = tr.in1 & tr.in2;
+                OR: expected = tr.in1 | tr.in2;
+                XOR: expected = tr.in1 ^ tr.in2;
+                SLL: expected = tr.in1 << tr.in2[4:0];
+                SRL: expected = tr.in1 >> tr.in2[4:0];
+                SRA: expected = $signed(tr.in1) >>> tr.in2[4:0];
+                SLT: expected = ($signed(tr.in1) < $signed(tr.in2));
+                SLTU: expected = (tr.in1 < tr.in2);
+                default: $fatal("Unknown ALU operation");
+            endcase
+
+            total_count++;
+            if(expected === tr.out) begin
+                pass_count++;
+
+                $display(
+                    "[PASS] %s in1=%h in2=%h expected=%h actual=%h",
+                    tr.aluOp.name(),
+                    tr.in1,
+                    tr.in2,
+                    expected,
+                    tr.out
+                );
+            end
+            else begin
+                fail_count++;
+
+                $display(
+                    "[FAIL] %s in1=%h in2=%h expected=%h actual=%h",
+                    tr.aluOp.name(),
+                    tr.in1,
+                    tr.in2,
+                    expected,
+                    tr.out
+                );
+            end
+        end
+
+        $display("");
+        $display("Total Tests : %0d", total_count);
+        $display("Passed      : %0d", pass_count);
+        $display("Failed      : %0d", fail_count);
+        -> done;
+    endtask
+endclass
+
+class env #(parameter WIDTH=32);
+    generator #(WIDTH) gen;
+    driver #(WIDTH) drv;
+    monitor #(WIDTH) mon;
+    scoreboard #(WIDTH) sb;
+
+    mailbox #(alu_transaction #(WIDTH)) drv_mb;
+    mailbox #(alu_transaction #(WIDTH)) mon_mb;
+
+    event drv_done;
+
+    virtual alu_if #(WIDTH) vif;
+
+    function new(virtual alu_if #(WIDTH) vif);
+        this.vif = vif;
+    endfunction
+
+    task build();
+        drv_mb = new();
+        mon_mb = new();
+        
+        gen = new(drv_mb, drv_done);
+        drv = new(vif, drv_mb, drv_done);
+        mon = new(vif, mon_mb, drv_done);
+        sb = new(mon_mb);
+    endtask
+
+    task run(int num_transactions = 100);
+        fork
+           gen.run(num_transactions);
+           drv.run();
+           mon.run();
+           sb.run(num_transactions);
+        join_none
+    endtask
+endclass
+
+class test #(parameter WIDTH=32);
+    env #(WIDTH) e;
+    virtual alu_if #(WIDTH) vif;
+
+    function new(virtual alu_if #(WIDTH) vif);
+        this.vif = vif;
+    endfunction
+
+    task run(int num_transactions = 100);
+        e = new(vif);
+        e.build();
+        e.run(num_transactions);
+    endtask
+endclass
+
+module alu_tb;
+    parameter WIDTH = 32;
+    localparam int NUM_TESTS = 100;
+
+    alu_if #(WIDTH) intf();
+    alu #(.WIDTH(WIDTH)) dut (
+        .in1(intf.in1),
+        .in2(intf.in2),
+        .aluOp(intf.aluOp),
+        .out(intf.out)
     );
 
+    test #(WIDTH) t;
 
-
-
+    initial begin
+        t = new(intf);
+        t.run(NUM_TESTS);
+        @(t.e.sb.done);
+        $finish;
+    end
 endmodule
